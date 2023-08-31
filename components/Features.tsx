0fe6ecbd395dec800f70d1e4d1b9e3b1c0a95b6a
samiramrullah <samiramrullah@gmail.com>
2023-08-31 18:07:58 +0530

    image unoptimized

--- components/Features.tsx
+++ components/Features.tsx
@@ -18,7 +18,7 @@ const Features = () => {
                 </div>
                 <div className="grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
                     <Link href={'/'} className="group relative flex h-48 flex-col overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-64 xl:h-96">
-                        <Image src={bbGlowTreatment} alt="BB GlowTreatment" layout="fill" objectFit="cover" className="group-hover:scale-110" />
+                        <img src={bbGlowTreatment} alt="BB GlowTreatment" layout="fill" objectFit="cover" className="group-hover:scale-110" />
                         <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 to-transparent md:via-transparent"></div>
                         <div className="relative mt-auto p-4">
                             <h2 className="mb-2 text-xl font-semibold text-white transition duration-100">Discover the Magic of BB Glow Treatment</h2>
@@ -26,7 +26,7 @@ const Features = () => {
                         </div>
                     </Link>
                     <Link href={'/pricing'} className="group relative flex h-48 flex-col overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-64 xl:h-96">
-                        <Image src={microBlading} alt="MicroBlading" layout="fill" objectFit="cover" className="group-hover:scale-110" />
+                        <img src={microBlading} alt="MicroBlading" layout="fill" objectFit="cover" className="group-hover:scale-110" />
                         <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 to-transparent md:via-transparent"></div>
                         <div className="relative mt-auto p-4">
                             {/* <span className="block text-sm text-gray-200">July 19, 2021</span> */}
