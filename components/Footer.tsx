d408efdcebc095e660a06b08f2bc5e3427850455
samiramrullah <samiramrullah@gmail.com>
2025-10-03 12:04:54 +0530

    foother @ changed to 2023

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -62,7 +62,7 @@ const Footer = () => {
         </div>
         <hr className="h-px my-6 bg-gray-200 border-none " />
         <div>
-          <p className="text-center text-gray-500 ">© Beauty Salon 2025 - All rights reserved</p>
+          <p className="text-center text-gray-500 ">© Beauty Salon 2023 - All rights reserved</p>
         </div>
       </div>
     </footer>
